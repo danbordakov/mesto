2953d2f0c3e751114e555559fc5caa388694b63f
danbordakov <danbordakov@yandex.ru>
2023-07-14 13:25:55 +0300

    fix: задание минимальной высоты контейнера карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,106 +1,102 @@
-const editButton = document.querySelector('.profile__edit-button');
-const newItemButton = document.querySelector('.profile__add-button');
-const popup = document.querySelector('.popup');
-const newItemPopup = document.querySelector('.popup_type_newitem');
-const fullviewPopup = document.querySelector('.popup_type_fullview');
-const closeButton = popup.querySelector('.popup__button-close');
-const closeNewItemButton = newItemPopup.querySelector('.popup__button-close_type_newitem');
-const closeFullviewButton = fullviewPopup.querySelector('.popup__button-close_type_fullview');
-const createButton = newItemPopup.querySelector('.popup__button-submit_type_create');
-const submitButton = popup.querySelector('.popup__button-submit');
-const elements = document.querySelector('.elements');
-const elementTemplate = document.querySelector('#element-template').content;
-const nameMain = document.querySelector('.profile__name');
+const popupEditInfo = document.querySelector('.popup_type_editinfo');
+const formEditInfo = popupEditInfo.querySelector('.popup__form');
+const popupNewItem = document.querySelector('.popup_type_newitem');
+const formNewItem = popupNewItem.querySelector('.popup__form');
+const popupFullview = document.querySelector('.popup_type_fullview');
+
+const buttonEditInfo = document.querySelector('.profile__edit-button'); 
+const buttonCloseEditInfo = popupEditInfo.querySelector('.popup__button-close');
+const buttonNewItem = document.querySelector('.profile__add-button');
+const buttonCloseNewItem = popupNewItem.querySelector('.popup__button-close');  
+const buttonCloseFullview = popupFullview.querySelector('.popup__button-close');
+ 
+const inputName = formEditInfo.querySelector('.popup__field_type_name'); 
+const inputJob = formEditInfo.querySelector('.popup__field_type_job'); 
+const nameMain = document.querySelector('.profile__name'); 
 const jobMain = document.querySelector('.profile__description');
+const inputNewItemName = formNewItem.querySelector('.popup__field_type_newitem-name');
+const inputNewItemLink = formNewItem.querySelector('.popup__field_type_newitem-link');
 
-let formElement = popup.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__field_type_name');
-let jobInput = formElement.querySelector('.popup__field_type_job');
-let formNewItem = newItemPopup.querySelector('.popup__form_type_newitem');
-let itemNameInput = formNewItem.querySelector('.popup__field_type_newitem-name');
-let itemLinkInput = formNewItem.querySelector('.popup__field_type_newitem-link');
+const elementTemplate = document.querySelector('#element-template').content;
+const elements = document.querySelector('.elements');
+const imageFullview = popupFullview.querySelector('.popup__image');
+const nameFullview = popupFullview.querySelector('.popup__image-name');
 
-//вставка первых карточек
-initialCards.forEach(function(card) {
-  const element = elementTemplate.querySelector('.element').cloneNode(true);
-  element.querySelector('.element__image').src = card.link;
-  element.querySelector('.element__description').textContent = card.name;
-  element.querySelector('.element__button-like').addEventListener('click', function(evt){
-    evt.target.classList.toggle('element__button-like_active');
-  });
-  element.querySelector('.element__button-trash').addEventListener('click', function(){
-    element.remove();
-  });
-  element.querySelector('.element__image').addEventListener('click', () => openFullviewImage(card.name, card.link));
-  elements.append(element);
+function openPopup(popupType) { 
+  popupType.classList.add('popup_opened'); 
+  inputName.value = nameMain.textContent; 
+  inputJob.value = jobMain.textContent; 
+} 
+
+function closePopup(popupType) { 
+  popupType.classList.remove('popup_opened'); 
+} 
+
+//изменение данных пользователя через форму
+function handleFormSubmit (evt) { 
+  evt.preventDefault(); 
+  nameMain.textContent = inputName.value; 
+  jobMain.textContent = inputJob.value; 
+  closePopup(popupEditInfo); 
+}
+
+//функция создания карточки
+function createCard(cardName, cardLink){
+  const card = elementTemplate.querySelector('.element').cloneNode(true);
+  card.querySelector('.element__image').src = cardLink;
+  card.querySelector('.element__image').alt = cardName;
+  card.querySelector('.element__description').textContent = cardName;
+  likeCard(card);
+  deleteCard(card);
+  openFullviewImage(card);
+  return card;
+}
+
+//функция добавления карточки на страницу
+function addCard(cardCreation){
+  elements.prepend(cardCreation);
+}
+
+//добавление первых карточек по умолчанию
+initialCards.forEach(function(card){
+  addCard(createCard(card.name, card.link));
 });
 
-function openPopup() {
-  popup.classList.add('popup_opened');
-  nameInput.value = nameMain.textContent;
-  jobInput.value = jobMain.textContent;
-}
-
-function openNewItemPopup() {
-  newItemPopup.classList.add('popup_opened');
-}
-
-function openNewItemPopup() {
-  newItemPopup.classList.add('popup_opened');
-}
-
-function openFullviewImage(cardname, cardlink){
-  fullviewPopup.classList.add('popup_opened');
-  fullviewPopup.querySelector('.popup__image').src = cardlink;
-  fullviewPopup.querySelector('.popup__image-name').textContent = cardname;
-}
-
-function closePopup() {
-  popup.classList.remove('popup_opened');
-}
-
-function closeNewItemPopup() {
-  newItemPopup.classList.remove('popup_opened');
-}
-
-function closeFullviewImage(){
-  fullviewPopup.classList.remove('popup_opened');
-}
-
-function handleFormSubmit (evt) {
+//добавление новой карточки через форму
+function addCardSubmit(evt) {
   evt.preventDefault();
-    nameMain.textContent = nameInput.value;
-    jobMain.textContent = jobInput.value;
-    closePopup();
+  addCard(createCard(inputNewItemName.value, inputNewItemLink.value));
+  closePopup(popupNewItem);
 }
 
-//добавление новых карточек
-function createNewCard(evt) {
-  evt.preventDefault();
-  if ((itemLinkInput.value === '') && (itemNameInput.value === '')) {
-    closeNewItemPopup();
-  } else {
-    const element = elementTemplate.querySelector('.element').cloneNode(true);
-    element.querySelector('.element__image').src = itemLinkInput.value;
-    element.querySelector('.element__description').textContent = itemNameInput.value;
-    element.querySelector('.element__button-like').addEventListener('click', function(evt){
-      evt.target.classList.toggle('element__button-like_active');
-    });
-    element.querySelector('.element__button-trash').addEventListener('click', function(){
-      element.remove();
-    });
-    element.querySelector('.element__image').addEventListener('click', () => openFullviewImage(element.querySelector('.element__description').textContent, element.querySelector('.element__image').src));
-    elements.prepend(element);
-    itemLinkInput.value = '';
-    itemNameInput.value = '';
-    closeNewItemPopup();
-  }
+//функция лайка карточки
+function likeCard(card){
+  card.querySelector('.element__button-like').addEventListener('click', function(evt){
+    evt.target.classList.toggle('element__button-like_active');
+  });
 }
 
-newItemButton.addEventListener('click', openNewItemPopup);
-closeNewItemButton.addEventListener('click', closeNewItemPopup);
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-closeFullviewButton.addEventListener('click', closeFullviewImage);
-formNewItem.addEventListener('submit', createNewCard);
-formElement.addEventListener('submit', handleFormSubmit);
+//функция удаления карточки
+function deleteCard(card){
+  card.querySelector('.element__button-trash').addEventListener('click', function(){
+    card.remove();
+  });
+}
+
+//функция открытия полного изображения
+function openFullviewImage(card){
+  card.querySelector('.element__image').addEventListener('click', ()=>{
+    openPopup(popupFullview);
+    imageFullview.src = card.querySelector('.element__image').src;
+    imageFullview.alt = card.querySelector('.element__image').src;
+    nameFullview.textContent = card.querySelector('.element__description').textContent;
+  });
+}
+
+buttonEditInfo.addEventListener('click', ()=>openPopup(popupEditInfo)); 
+buttonCloseEditInfo.addEventListener('click', ()=>closePopup(popupEditInfo));
+buttonNewItem.addEventListener('click', ()=>openPopup(popupNewItem));
+buttonCloseNewItem.addEventListener('click', ()=>closePopup(popupNewItem));
+buttonCloseFullview.addEventListener('click', ()=>closePopup(popupFullview));
+formEditInfo.addEventListener('submit', handleFormSubmit);
+formNewItem.addEventListener('submit', addCardSubmit);

5ac61d4a901be5ac6eb6aada09e4fc1123059d07
danbordakov <danbordakov@yandex.ru>
2023-07-27 14:42:14 +0300

    upd: validation config extended

---
+++ scripts/constants.js
@@ -0,0 +1,8 @@
+config = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__field',
+  submitButtonSelector: '.popup__button-submit',
+  inactiveButtonClass: 'popup__button-submit_type_unenabled',
+  inputErrorClass: 'popup__field_type_error',
+  errorClass: 'popup__field-error_visible'
+};
